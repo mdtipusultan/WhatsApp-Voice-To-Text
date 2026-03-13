9bac8c874d62ea65e7c28b69bfe5ccf5c27a917e
Md. Tipu <tipu.diu.cse@gmail.com>
2026-03-13 16:30:13 +0600

    project added
    project added

---
+++ WhatsApp-Voice-To-Text/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhatsApp-Voice-To-Text
+//
+//  Created by MacBook Pro M1 Pro on 3/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhatsApp-Voice-To-Text/WhatsApp_Voice_To_TextApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsApp_Voice_To_TextApp.swift
+//  WhatsApp-Voice-To-Text
+//
+//  Created by MacBook Pro M1 Pro on 3/13/26.
+//
+
+import SwiftUI
+
+@main
+struct WhatsApp_Voice_To_TextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
